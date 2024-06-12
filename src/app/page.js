c44f6cfe7562c5c1b147973681d3f26aad6bf148
Aryan Singh <aryan_singh@brown.edu>
2024-06-11 21:55:48 -0400

    todo: remove uncessary files, make dimensions absolute and render for tablet/mobile

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,37 @@ export default function Home() {
       <Navbar />
       <section className={styles.section1}></section>
       <section className={styles.section2}></section>
-      <section className={styles.section3}></section>
+      <section className={styles.section3}>
+        <h2 className={styles.subheading}>How It Works</h2>
+        <div className={styles.steps}>
+          <div className={styles.step}>
+            <div className={styles.stepHeader}>
+              <div className={styles.stepNumber}>1</div>
+              <h3 className={styles.stepTitle}>Sponsors post IT projects</h3>
+            </div>
+            <Image
+              src="/how_it_works_left.svg" // Replace with your actual image path
+              alt="Left Image"
+              width={280}
+              height={240}
+              className={styles.stepImage}
+            />
+          </div>
+          <div className={styles.step}>
+            <div className={styles.stepHeader}>
+              <div className={styles.stepNumber}>2</div>
+              <h3 className={styles.stepTitle}>IT Talent apply for projects</h3>
+            </div>
+            <Image
+              src="/how_it_works_right.svg" // Replace with your actual image path
+              alt="Right Image"
+              width={304}
+              height={240}
+              className={styles.stepImage}
+            />
+          </div>
+        </div>
+      </section>
       <section className={styles.section4}></section>
     </main>
   );
